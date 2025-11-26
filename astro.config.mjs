4c78226d4b6ba91034da8f4c981d970653ecd367
Rabboni Kabongo <kabongorabboni03@gmail.com>
2025-11-26 12:30:47 +0200

    fix: I have updated astro.config.mjs to use the Vercel Adapter (@astrojs/vercel).

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import tailwindcss from '@tailwindcss/vite';
 import svelte from '@astrojs/svelte';
 
 import mdx from '@astrojs/mdx';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
   },
